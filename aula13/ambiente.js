f97c74552c7eae5ec902794afc9a81163b50064b
luizeh <luisricardosoares34@gmail.com>
2026-05-08 17:24:13 -0300

    aula 13 e aula 14 adicionados

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+// var c = 1
+// do{
+//     console.log(`pagotti ${c}`)
+//     c++
+// } while (c<=7)
+
+
+for(var c = 1; c<=7; c++){
+    console.log(`pagotti ${c}`)
+}
+
+// while (c <=7){
+//     console.log(`pagotti ${c}`)
+//     c++
+// }
+
+
+// console.log('pagotti')
+// console.log('pagotti')
+// console.log('pagotti')
+// console.log('pagotti')
+// console.log('pagotti')
+// console.log('pagotti')
+// console.log('pagotti')
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('vai começar...')
+for(var c = 10; c>0; c--){
+    console.log(`pagotti ${c}`)
+}
+console.log('FIM!') 
